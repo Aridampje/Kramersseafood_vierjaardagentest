ae6bae9afc3988dcf765f9c7e5aa6430a92ff668
Arjan Wemmers <161522826+Aridampje@users.noreply.github.com>
2026-08-26 15:03:22 +0200

    Update volgend_maand.js

--- volgend_maand.js
+++ volgend_maand.js
@@ -10,7 +10,9 @@ fetch('./mockup_api/nepapi_werknemers.json')
 .then(response => response.json())
 .then(data => {
     let volgend_maand_titel = document.getElementById("volgend_maand_titel");
-    let volgend_maand_lijst = document.getElementById("volgend_maand_lijst");
+    let volgend_maand_geboortedag = document.getElementById("volgend_maand_geboortedag");
+    let volgend_maand_naam = document.getElementById("volgend_maand_naam");
+    let volgend_maand_leeftijd = document.getElementById("volgend_maand_leeftijd");
 
     data.werknemers.sort((a,b) => {
         return new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate();
@@ -25,10 +27,9 @@ fetch('./mockup_api/nepapi_werknemers.json')
         volgend_maand_titel.textContent =      //VOLGENDE MAAND
         `Volgend maand jarig (${volgend_maand_in_tekst})`;
         if(geboortemaand == huidige_maand + 1){
-            let birthdaylijst = document.createElement("h3");
-            birthdaylijst.textContent = `${persoon.firstName} ${persoon.lastName}
-            is om ${geboortedag} ${volgend_maand_in_tekst} jarig hij word ${hoe_oud_geworden}`;
-            volgend_maand_lijst.appendChild(birthdaylijst)
+            volgend_maand_geboortedag.innerHTML += `<h3>${geboortedag} </h3>`
+            volgend_maand_naam.innerHTML += `<h3>${persoon.firstName} ${persoon.lastName}</h3>`
+            volgend_maand_leeftijd.innerHTML += `<h3>${hoe_oud_geworden} </h3>`
         }
     });
 })
